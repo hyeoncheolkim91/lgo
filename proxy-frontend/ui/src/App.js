b59b6c2ddf9b4c851390970d3c768bba8360cf0d
jason.kim <jason.kim@advantest.com>
2021-05-28 11:10:59 -0500

    new

--- proxy-frontend/ui/src/App.js
+++ proxy-frontend/ui/src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from "react";
-import logo from "./AdvantestLogo.png";
 import "./App.css";
-import AuthService from "./components/AuthService";
+// import AuthService from "./components/AuthService";
 import withAuth from "./components/withAuth";
-
+import Appbar from "./components/bar"
 
 class App extends Component {
   constructor() {
     super();
     
-    this.Auth = new AuthService();
+    
   }
 
-  handleLogout(e) {
-    e.preventDefault();
-
-    this.Auth.logout(this.props.user.username)
-      .then((res) => {
-        this.props.history.replace("/login");
-      })
-      .catch((err) => {
-        alert(err);
-      });
-  }
+ 
 
 
   render() {
@@ -30,24 +19,13 @@ class App extends Component {
     
     return (
       <div className="App">
+      <Appbar></Appbar>
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome Voice2021</h2>
+          <div className="brstyle"></div>
           <div className="container"> 
-            <iframe className="responsive-iframe" src={this.props.url} />
-          </div>
-            
-          <button
-            type="button"
-            className="form-submit"
-            onClick={this.handleLogout.bind(this)}
-          >
-            Logout
-          </button>
+          <iframe className="responsive-iframe" src={this.props.url} />
+          </div>          
         </div>
-        <p className="App-intro">
-          
-        </p>
       </div>
     );
   }
---
+++ proxy-frontend/ui/src/components/bar.js
@@ -0,0 +1,84 @@
+import React,{Component} from "react";
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import logo from "../AdvantestLogo.png";
+import AuthService from "./AuthService";
+import withAuth from "./withAuth";
+
+const useStyles = withStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 2,
+    },
+    appbar:{
+      minHeight: 64
+    },
+    menuButton: {
+      marginRight: theme.spacing(3),
+    },
+    logo: {
+      marginBottom: 2,
+      minWidth: "10%",
+    },
+  })
+);
+
+class Bar extends Component {
+  constructor() {
+    super();
+    
+    this.Auth = new AuthService();
+  }
+
+  handleLogout(e) {
+    this.Auth.logout(this.props.user.username)
+      .then((res) => {
+        window.location.reload();
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+
+  render() {
+
+    const classes = useStyles
+    return (
+      <div className={classes.root}>
+      <AppBar position="static" color="inherit">
+        <Toolbar className={classes.appbar} >
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+           
+          >
+            <img src={logo} alt="advantest" className={classes.logo} />
+
+            <Button
+                onClick={this.handleLogout.bind(this)}
+              style={{
+                borderRadius: 20,     
+                fontWeight:'bold',
+                color: "#8E0041",
+                fontSize: "18px",
+                marginRight:'10px'
+              }}
+              variant="text"
+            >
+              Logout
+            </Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+    );
+  }
+}
+
+export default withAuth(Bar);
